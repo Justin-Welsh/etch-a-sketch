c0803edd99c1fbb72f1853a1e6c309b1608cd0a0
Justin <jwelsh518@gmail.com>
2020-08-17 19:05:50 -0400

    Figured out a very rough and cobbled together solution. Marking as finished

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-/*
-    Board size is 512px
-    Each square needs to be 32px
-*/
+let root = document.documentElement;
+let container = document.querySelector("#container");
+let button = document.getElementById("button");
 
 
-const board = document.querySelector(".board");
-
-function populateBoard() {
-    var i = 0;
-    
-    
-    // create 16 rows within the board div
-    while(i < 16) {
-        var row = document.createElement("div");
-        row.setAttribute("id", "row");
-        board.classList.add("row");
-        board.appendChild(row);
-
-        i++;
+// Fill the board with a grid of squares
+function populateBoard(numberOfTiles) {
+    let num = getComputedStyle(root).getPropertyValue('--num-boxes');
+    for(let i = 0; i < num ** 2; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
     }
-/* 
-    for(var j = 0; j < 16; j++) {
-        var square = document.createElement("div");
-        square.setAttribute("class", "square");
-        row.classList.add("square");
-        row.appendChild(square);
-        } */
-    
-
-    
-
-
-    const rows = document.querySelectorAll("#row");
-
-    
-    rows.forEach((row) => {
-        for(var j = 0; j < 16; j++) {
-            var square = document.createElement("div");
-            square.setAttribute("class", "square");
-            row.classList.add("square");
-            row.appendChild(square);
-            }
-        
-    });
-    
-
-    
 }
 
+// Add event listeners to each square within the board container
+function squareEventListeners() {
+    let squareList = document.querySelectorAll(".square");   
+    squareList.forEach(square => square.addEventListener('mouseenter', function() {
+        this.style.backgroundColor = "#3f3f3f";
+    }));    
+}
+
+// Clear the board when you press the button
+function clearBoard() {
+    let squareList = document.querySelectorAll(".square");
+    squareList.forEach(square => square.style.backgroundColor = "#fff");
+}
+
+// Populate the board, add the event listeners and start the sketch
 populateBoard();
+squareEventListeners();
+button.addEventListener("click", clearBoard);
